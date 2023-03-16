c4d9428af21596b00207ae38bcfa4019c2ca121b
Lucas Mendonça da Silva junior <lucasjr413@gmail.com>
2023-03-15 21:49:40 -0300

    Add Exercises 1-4

---
+++ 2-Semestre/ED/Aula04/Ex04.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <pilha.h>
+#include <stdlib.h>
+
+using namespace std;
+
+Pilha pilha; 
+
+int main(){
+	IniPilha(&pilha); 
+	int num,resto = 0; 
+	
+	cout << "\nDigite um número decimal: ";
+	cin>> num;
+	
+	
+	return 0; 
+}
