becd3622d4b66a5fda5466e17ce6417cf3623a3d
Lucas Mendonça da Silva Juinior <lucasjr413@gmail.com>
2023-03-22 20:04:33 -0300

    Add Lista 04 (Finalizada)

--- 2-Semestre/ED/Aula04/Ex05.cpp
+++ 2-Semestre/ED/Aula04/Ex05.cpp
@@ -1,16 +1,35 @@
+/*
+
+*/
+
 #include <iostream>
 #include <pilha.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string>
-#include <stack>
 
-
-using namespace std;
- 	
+using namespace std; 	
 
 int main(){	
 
+	Pilha pilha; 
+	IniPilha(&pilha);
+	
+	int n, t;
+	char element;
+	char palavra[25];
+  
+	
+	cout << "Digite uma palavra: \n"; 
+	cin >> palavra; 
+	
+	n = strlen(palavra);
+	for( int i = 0; i < n; i++){
+		Push(&pilha,palavra[i]); 
+	}
+	while(!pilhavazia(&pilha)){
+		element = Pop(&pilha);
+		cout << element;
+	}
+	
+	
 	
 	
 	return 0; 
